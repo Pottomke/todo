789d71aa252c5bfbfc1b350acdfd7b8e6365c83b
Pottomke <vas.petra13@gmail.com>
2022-03-28 08:31:10 +0200

    basics

---
+++ react_todo/src/components/Note.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Note = ({}) => {
+    //Events
+   
+    return(
+        <div className="note">
+            
+        </div>
+    );
+};
+
+export default Note;
